9654d5d64439be80661bfa741349b1de8ac1edd5
Nate J.M <nate@natejonah.com>
2023-09-01 23:49:42 +0100

    Add App starting content

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,49 @@ function App() {
 
     return (
         <>
-            <h1>Password Generator</h1>
+            <header>
+                <h1>Password Generator</h1>
+            </header>
+
+            <main>
+                <div>
+                    <h2>P4$5W0rD!</h2>
+                </div>
+
+                <div>
+                    <form>
+                        <label htmlFor="length">Character Length</label>
+                        <input type="range" name="length" id="length" min={1} max={24} step={1} />
+                        <span className="characterLength">0</span>
+
+                        <input type="checkbox" id="uppercase" name="uppercase" />
+                        <label htmlFor="uppercase">Include Uppercase Letters</label>
+
+                        <input type="checkbox" id="lowercase" name="lowercase" />
+                        <label htmlFor="lowercase">Include Lowercase Letters</label>
+
+                        <input type="checkbox" id="numbers" name="numbers" />
+                        <label htmlFor="numbers">Include Numbers</label>
+
+                        <input type="checkbox" id="symbols" name="symbols" />
+                        <label htmlFor="symbols">Include Symbols</label>
+
+                        <div>
+                            <span>Strength</span>
+
+                            <div>
+                                <span>Medium</span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+
+                        <button type="submit">Generate</button>
+                    </form>
+                </div>
+            </main>
         </>
     )
 }
