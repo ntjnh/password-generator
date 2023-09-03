80d29e3d542166fd68f270ef7378b046244aab56
Nate J.M <nate@natejonah.com>
2023-09-03 22:47:31 +0100

    Add classes for styling and create password state

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,25 @@
+import { useState } from 'react'
+import Form from './assets/components/Form'
 import './assets/scss/app.scss'
+import copyIcon from './assets/images/copy.png'
 
 function App() {
+    const [password, setPassword] = useState('P4$5W0rD!')
 
     return (
         <div className="App">
-            <header>
-                <h1>Password Generator</h1>
-            </header>
-
             <main>
-                <div>
-                    <h2>P4$5W0rD!</h2>
+                <h1>Password Generator</h1>
+
+                <div className="password">
+                    <h2 className="password__text">{password}</h2>
+
+                    <button className="password__copy">
+                        <img src={copyIcon} alt="Copy password" />
+                    </button>
                 </div>
 
-                <div>
-                    <form>
-                        <label htmlFor="length">Character Length</label>
-                        <input type="range" name="length" id="length" min={1} max={24} step={1} />
-                        <span className="characterLength">0</span>
-
-                        <input type="checkbox" id="uppercase" name="uppercase" />
-                        <label htmlFor="uppercase">Include Uppercase Letters</label>
-
-                        <input type="checkbox" id="lowercase" name="lowercase" />
-                        <label htmlFor="lowercase">Include Lowercase Letters</label>
-
-                        <input type="checkbox" id="numbers" name="numbers" />
-                        <label htmlFor="numbers">Include Numbers</label>
-
-                        <input type="checkbox" id="symbols" name="symbols" />
-                        <label htmlFor="symbols">Include Symbols</label>
-
-                        <div>
-                            <span>Strength</span>
-
-                            <div>
-                                <span>Medium</span>
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
-                        </div>
-
-                        <button type="submit">Generate</button>
-                    </form>
-                </div>
+                <Form />
             </main>
         </div>
     )
