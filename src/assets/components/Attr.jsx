2513f09097fc414e58d0f7e218c4c2e729a62e78
Nate <codebynate@gmail.com>
2025-02-17 01:25:40 +0000

    Update Attribution component with TW classes

--- src/assets/components/Attr.jsx
+++ src/assets/components/Attr.jsx
@@ -1,11 +1,9 @@
-import './Attr.scss'
-
 export default function Attr() {
     return (
-        <footer className="attribution">
-            <div>
-                Challenge by <br /><a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>; <br />
-                Coded by <a href="https://nate-dev.com?utm_source=codebynate&utm_medium=web&utm_campaign=password_generator" target="_blank" rel="noreferrer">Nate</a>
+        <footer className="bg-neutral-825 md:bottom-6 md:fixed font-normal leading-normal mb-0 mt-8 md:mt-0 mx-0 md:right-6 text-center text-xs text-white">
+            <div className="mx-auto my-0 p-4">
+                Challenge by <br className="hidden md:inline" /><a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>; <br />
+                Coded by <a className="no-underline hover:underline text-green-250" href="https://nate-dev.com?utm_source=codebynate&utm_medium=web&utm_campaign=password_generator" target="_blank" rel="noreferrer">Nate</a>
             </div>
         </footer>
     )
